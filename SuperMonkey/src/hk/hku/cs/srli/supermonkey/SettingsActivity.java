470e0a5253c26eee3fb475b0cbe428c5d5fa8912
LeeThree <leethree9@gmail.com>
2012-10-19 14:24:47 +0800

    Used a generized method to gen pref summaries

--- SuperMonkey/src/hk/hku/cs/srli/supermonkey/SettingsActivity.java
+++ SuperMonkey/src/hk/hku/cs/srli/supermonkey/SettingsActivity.java
@@ -5,6 +5,8 @@ import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -34,12 +36,19 @@ public class SettingsActivity extends Activity {
     }
 
     public static class SettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener{
+    	
+    	private PreferenceScreen ps;
+    	
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.preferences);
+            
+            ps = getPreferenceScreen();
+            
+            initSummaries(ps);
         }
 
         @Override
@@ -47,7 +56,7 @@ public class SettingsActivity extends Activity {
         	super.onResume();
         	
         	// Set up a listener whenever a key changes
-            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+            ps.getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
         }
         
         @Override
@@ -55,21 +64,34 @@ public class SettingsActivity extends Activity {
         	super.onPause();
         	
         	// Unregister the listener whenever a key changes
-            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+            ps.getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
         }
         
 		@Override
 		public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-			Preference pref = findPreference(key);
-
-		    if (pref instanceof ListPreference) {
-		        ListPreference listPref = (ListPreference) pref;
-		        pref.setSummary(listPref.getEntry());
-		    } else if (pref instanceof EditTextPreference) {
-		    	EditTextPreference etPref = (EditTextPreference) pref;
-		    	pref.setSummary(etPref.getText());
-		    }
+			updateSummary(findPreference(key));
 		}
+		
+		private void initSummaries(PreferenceGroup pg) {
+			for(int i = 0; i < pg.getPreferenceCount(); i++) {
+				Preference p = pg.getPreference(i);
+				if (p instanceof PreferenceGroup)
+					initSummaries((PreferenceGroup) p);
+				else
+					updateSummary(p);
+            }
+        }
+
+        private void updateSummary(Preference p) {
+        	if (p instanceof ListPreference) {
+                ListPreference listPref = (ListPreference) p; 
+                p.setSummary(listPref.getEntry()); 
+            }
+            if (p instanceof EditTextPreference) {
+                EditTextPreference editTextPref = (EditTextPreference) p; 
+                p.setSummary(editTextPref.getText()); 
+            }
+        }
     }
     
 }
