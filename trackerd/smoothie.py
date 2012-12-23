8e30dccf7d73d9a3e46f6894861da5521c2c33bb
LeeThree <leethree9@gmail.com>
2012-12-23 21:52:21 +0800

    Implemented offline FIR filters.

--- trackerd/smoothie.py
+++ trackerd/smoothie.py
@@ -18,7 +18,7 @@ class Gaze(object):
         self.validity = long() # Validity
 
     def __str__(self):
-        return ('%d:' % ((self.t - 1167612915647489) / 29059) +
+        return ('%.2f:' % ((self.t - 1167612915647489) / 29059.0) +
                 '%s|' % _p3d(self.h) +
                 '%s,%s,' % (_p3d(self.p), _p2d(self.p2d)) +
                 '%.2f,%d' % (self.pupil, self.validity)
@@ -32,18 +32,18 @@ def _p3d(p):
 
 
 def get_data():
-    file = open('data.pk', 'rb')
+    data_file = open('data.pk', 'rb')
 
     raw_data = []
 
     try:
         while(True):
-            gaze = pickle.load(file)
+            gaze = pickle.load(data_file)
             raw_data.append(gaze)
     except EOFError:
         print "EOF"
 
-    file.close()
+    data_file.close()
 
     data = []
 
@@ -76,14 +76,62 @@ def dot(v1, v2):
     return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
 
 def main():
+    # Savitzky-Golay smoothing filters
+    filter_h = [-3, 12, 17, 12, -3]
+    filter_h = [x / 35.0 for x in filter_h]
+    filter_g = [-3, -2, -1, 0, 1, 2, 3]
+    filter_g = [x / 28.0 for x in filter_g]
+
     data = get_data()
     lastv = None
+    lastt = None
+    theta = []
+    deltat = []
     for (left, right) in data:
         v = sub(left.p, left.h)
-        if lastv is not None:
-            cos = dot(v, lastv) / math.sqrt(dot(v, v) * dot(lastv, lastv))
-            print math.degrees(math.acos(cos))
+        try:
+            if lastv is not None:
+                cos = dot(v, lastv) / math.sqrt(dot(v, v) * dot(lastv, lastv))
+                theta_i = math.degrees(math.acos(cos))
+                theta.append(theta_i)
+                deltat.append((left.t - lastt) / 1000000.0)
+        except ArithmeticError:
+            pass
         lastv = v
+        lastt = left.t
+
+    print 'deltat =\n', deltat
+    print 'theta =\n', theta
+
+    dtheta = []
+    for i in xrange(0, len(theta) - len(filter_h)):
+        dtheta_i = 0.0
+        for j in xrange(0, len(filter_h)):
+            dtheta_i = dtheta_i + theta[i + j] * filter_h[j]
+        dtheta.append(dtheta_i / deltat[i])
+
+    print 'dtheta =\n', dtheta
+
+    ddtheta = []
+    for i in xrange(0, len(dtheta) - len(filter_g)):
+        ddtheta_i = 0.0
+        for j in xrange(0, len(filter_g)):
+            ddtheta_i = ddtheta_i + dtheta[i + j] * filter_g[j]
+        ddtheta.append(ddtheta_i / deltat[i])
+
+    print 'ddtheta =\n', ddtheta
+
+    saccade = False
+    sign = False
+    for i in xrange(0, len(ddtheta)):
+        accel = math.fabs(ddtheta[i])
+        if accel > 300:
+            saccade = True
+            sign = True if ddtheta[i] > 0 else False
+        elif saccade and accel > 200:
+            saccade = False if (ddtheta[i] > 0) is not sign else saccade
+        print saccade
+
 
 
 if __name__ == '__main__':
