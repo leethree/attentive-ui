883b56e71ed7047b46a390bfcbdd9a79cfd35d2b
LeeThree <leethree9@gmail.com>
2012-12-30 20:57:55 +0800

    Added averaging for both eyes.

--- trackerd/smoothie.py
+++ trackerd/smoothie.py
@@ -5,8 +5,6 @@ import math
 import pickle
 from decimal import Decimal
 
-from tobii.sdk.types import Point2D, Point3D
-
 class P3(object):
 
     def __init__(self, x=0, y=0, z=0):
@@ -43,7 +41,7 @@ class P3(object):
 class Gaze(object):
 
     def __init__(self):
-        self.t = long() # Timestamp
+        self.t = float() # Timestamp
 
         self.h = P3() # EyePosition3D
         self.h_relative = P3() # EyePosition3DRelative
@@ -62,7 +60,7 @@ class Gaze(object):
     @staticmethod
     def of(gaze):
         lp = Gaze()
-        lp.t = gaze.Timestamp
+        lp.t = gaze.Timestamp / 1000000.0
         lp.h = P3.of(gaze.LeftEyePosition3D)
         lp.h_relative = P3.of(gaze.LeftEyePosition3DRelative)
         lp.p = P3.of(gaze.LeftGazePoint3D)
@@ -71,7 +69,7 @@ class Gaze(object):
         lp.validity = gaze.LeftValidity
 
         rp = Gaze()
-        rp.t = gaze.Timestamp
+        rp.t = gaze.Timestamp / 1000000.0
         rp.h = P3.of(gaze.RightEyePosition3D)
         rp.h_relative = P3.of(gaze.RightEyePosition3DRelative)
         rp.p = P3.of(gaze.RightGazePoint3D)
@@ -84,7 +82,7 @@ class Gaze(object):
 
 class FirFilter(object):
 
-    def __init__(self, filters, normalization):
+    def __init__(self, filters, normalization=1):
         self._filter = [x / normalization for x in filters]
         mem_len = len(self._filter)
         self._mem = collections.deque([0] * mem_len, maxlen=mem_len)
@@ -100,9 +98,32 @@ class FirFilter(object):
         return ret
 
 
+class Differentiator(object):
+
+    def __init__(self, default, diff_func=lambda x, y: x - y):
+        self._last_value = None
+        self._default_value = default
+        self._diff_func = diff_func
+
+    def clear(self):
+        self._last_value = None
+
+    def diff(self, value):
+        if self._last_value is not None:
+            ret = self._diff_func(value, self._last_value)
+        else:
+            ret = self._default_value
+        self._last_value = value
+        return ret
+
+
 class FixationDetector(object):
 
     def __init__(self):
+        self._time_diff = Differentiator(1)
+        self._left_diff = Differentiator(0, self._get_theta)
+        self._right_diff = Differentiator(0, self._get_theta)
+
         # Savitzky-Golay smoothing filters
         filter_h = [-3, 12, 17, 12, -3]
         self._velocity_filter = FirFilter(filter_h, 35.0)
@@ -111,6 +132,9 @@ class FixationDetector(object):
         self._init_params()
 
     def clear(self):
+        self._time_diff.clear()
+        self._left_diff.clear()
+        self._right_diff.clear()
         self._velocity_filter.clear()
         self._accel_filter.clear()
         self._init_params()
@@ -118,8 +142,21 @@ class FixationDetector(object):
     def is_fixation(self, data_item):
         self._counter += 1
         left, right = data_item
-        delta_t = self._get_delta_t(left)
-        theta = self._get_theta(left)
+
+        delta_t = self._time_diff.diff(left.t)
+
+        theta, weight = 0.0, 0
+        # use validity to determine weight of each eye in caculation
+        if left.validity < 2:
+            theta += self._left_diff.diff(left.p - left.h)
+            weight += 1
+        if right.validity < 2:
+            theta += self._right_diff.diff(right.p - right.h)
+            weight += 1
+
+        if weight > 0:
+            theta /= weight
+
         dtheta = self._velocity_filter.filter(theta) / delta_t
         ddtheta = self._accel_filter.filter(dtheta) / delta_t
         if self._saccade:
@@ -132,6 +169,9 @@ class FixationDetector(object):
             if ddtheta > 300:
                 self._saccade = True
                 self._last_counter = self._counter
+
+        print delta_t, theta, dtheta, ddtheta, 1 if self._saccade else 0
+
         return False if self._saccade else True # fixation if not in saccade
 
     def _init_params(self):
@@ -141,25 +181,12 @@ class FixationDetector(object):
         self._last_counter = 0
         self._saccade = False
 
-    def _get_delta_t(self, gaze):
-        if self._last_t is not None:
-            delta_t = (gaze.t - self._last_t) / 1000000.0
-        else:
-            delta_t = 1
-        self._last_t = gaze.t
-        return delta_t
-
-    def _get_theta(self, gaze):
-        v = gaze.p - gaze.h
-        theta = 0
-        if self._last_v is not None:
-            try:
-                cos = (v * self._last_v) / (abs(v) * abs(self._last_v))
-                theta = math.degrees(math.acos(cos))
-            except ArithmeticError:
-                pass
-        self._last_v = v
-        return theta
+    def _get_theta(self, v, last_v):
+        try:
+            cos = (v * last_v) / (abs(v) * abs(last_v))
+            return math.degrees(math.acos(cos))
+        except ArithmeticError:
+            return 0
 
 
 def get_data():
@@ -189,8 +216,6 @@ def main():
     s_indicator = []
     for item in get_data():
         s_indicator.append(0 if detector.is_fixation(item) else 1)
-
-    print 's_indicator =\n', s_indicator, len(s_indicator)
 
 
 if __name__ == '__main__':
