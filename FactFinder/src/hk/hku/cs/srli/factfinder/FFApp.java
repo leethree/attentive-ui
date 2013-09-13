5acbae9bee9c555b50829ef3847c29ac1254e860
LeeThree <leethree9@gmail.com>
2013-09-13 17:06:23 +0800

    Move config to FFApp.

--- FactFinder/src/hk/hku/cs/srli/factfinder/FFApp.java
+++ FactFinder/src/hk/hku/cs/srli/factfinder/FFApp.java
@@ -2,10 +2,28 @@ package hk.hku.cs.srli.factfinder;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FFApp extends Application {
 
+    public static final String FF_PREF = "FF_Pref";
+
+    public static final int APP_THEME = R.style.AppTheme;
+    public static final int APP_THEME_NO_HOVER = R.style.AppTheme_NoHover;
+    public static final List<Integer> TESTS = new ArrayList<Integer>(5);
+    
+    static {
+        TESTS.add(R.xml.duck);
+        TESTS.add(R.xml.burger);
+        TESTS.add(R.xml.cheesecake);
+        TESTS.add(R.xml.coffee);
+        TESTS.add(R.xml.hotpot);
+    }
+    
     private DataSet mData;
     private Order mOrder;
     private int mTheme = R.style.AppTheme;
@@ -16,28 +34,35 @@ public class FFApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        
         // initialize dataset
         mData = new DataSet(this, R.xml.empty);
         mOrder = new Order(this);
         mTheme = getApplicationInfo().theme;
-    }
-    
-    public void changeDataSet(int dataSource) {
-        mOrder.reset();
-        mData = new DataSet(this, dataSource);
+        
+        // load initial config from preferences
+        SharedPreferences pref = getSharedPreferences(FF_PREF, 0);
+        changeConfig(pref.getInt("ndataset", 0), pref.getBoolean("bhover", false));
     }
     
     public int getFFTheme() {
         return mTheme;
     }
     
-    public void setFFTheme(int resid) {
-        mTheme = resid;
+    public void changeConfig(int dataset, boolean hover) {
+        mOrder.reset();
+        mData = new DataSet(this, TESTS.get(dataset));
+        mTheme = hover ? APP_THEME : APP_THEME_NO_HOVER;
+        sLogPrefix = "D" + dataset + " H" + hover;
     }
     
     // Utility method to get a instance.
     public static FFApp getApp(Context context) {
         return (FFApp) context.getApplicationContext();
+    }
+    
+    public static SharedPreferences getPreferences(Context context) {
+        return getApp(context).getSharedPreferences(FF_PREF, 0);
     }
     
     public static DataSet getData(Context context) {
--- FactFinder/src/hk/hku/cs/srli/factfinder/TestActivity.java
+++ FactFinder/src/hk/hku/cs/srli/factfinder/TestActivity.java
@@ -8,35 +8,19 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.NumberPicker;
 import android.widget.Switch;
-import android.widget.TextView;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TestActivity extends Activity
         implements View.OnClickListener, NumberPicker.OnValueChangeListener, OnCheckedChangeListener {
 
-    private static final int APP_THEME = R.style.AppTheme;
-    private static final int APP_THEME_NO_HOVER = R.style.AppTheme_NoHover;
-    private static final List<Integer> TESTS = new ArrayList<Integer>(5);
-    
-    static {
-        TESTS.add(R.xml.duck);
-        TESTS.add(R.xml.burger);
-        TESTS.add(R.xml.cheesecake);
-        TESTS.add(R.xml.coffee);
-        TESTS.add(R.xml.hotpot);
-    }
     
     private static int sDataset = 0;
     private static boolean sHover = false;
     
     private NumberPicker npd;
     private Switch sh;
-    private TextView status;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +29,7 @@ public class TestActivity extends Activity
         
         npd = (NumberPicker) findViewById(R.id.number_picker_d);
         npd.setMinValue(0);
-        npd.setMaxValue(TESTS.size() - 1);
+        npd.setMaxValue(FFApp.TESTS.size() - 1);
         npd.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
         npd.setOnValueChangedListener(this);
         
@@ -55,10 +39,8 @@ public class TestActivity extends Activity
         Button start = (Button) findViewById(R.id.start_button);
         start.setOnClickListener(this);
         
-        status = (TextView) findViewById(R.id.text_status);
-        
         // restore data
-        SharedPreferences settings = getSharedPreferences("FF_Test", 0);
+        SharedPreferences settings = FFApp.getPreferences(this);
         sDataset = settings.getInt("ndataset", sDataset);
         sHover = settings.getBoolean("bhover", sHover);
     }
@@ -68,7 +50,6 @@ public class TestActivity extends Activity
         super.onResume();
         npd.setValue(sDataset);
         sh.setChecked(sHover);
-        prepareTest();
     }
     
     @Override
@@ -76,7 +57,7 @@ public class TestActivity extends Activity
         super.onStop();
         
         // save data
-        SharedPreferences settings = getSharedPreferences("FF_Test", 0);
+        SharedPreferences settings = FFApp.getPreferences(this);
         SharedPreferences.Editor editor = settings.edit();
         editor.putInt("ndataset", sDataset);
         editor.putBoolean("bhover", sHover);
@@ -85,8 +66,9 @@ public class TestActivity extends Activity
 
     @Override
     public void onClick(View v) {
-        FFApp.log("Test", "Trial starting: D" + sDataset + " H" + sHover +
-                " with config: "+ status.getText());
+        FFApp.log("Test", "Trial starting: D" + sDataset + " H" + sHover);
+        // change configuration
+        FFApp.getApp(this).changeConfig(sDataset, sHover);
         Intent i = new Intent(this, MainActivity.class);
         // clear activity stack
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -97,22 +79,11 @@ public class TestActivity extends Activity
     @Override
     public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
         sDataset = npd.getValue();
-        prepareTest();
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         sHover = sh.isChecked();
-        prepareTest();
     }
-    
-    private void prepareTest() {
-        // test sessions
-        int data = sDataset;
-        boolean hover = sHover;
-        FFApp.getApp(this).changeDataSet(TESTS.get(data));
-        FFApp.getApp(this).setFFTheme(hover ? APP_THEME : APP_THEME_NO_HOVER);
-        status.setText("d" + data + " h" + (hover ? 1 : 0));
-        FFApp.sLogPrefix = "D" + sDataset + " H" + sHover;
-    }
+
 }
